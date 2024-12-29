73c7a2a789e7fe29e9c608a6e0ce8b461ee6679a
Ari-Wisc <arinoson1@gmail.com>
2024-12-28 19:17:34 -0600

    allow triangle to move

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -7,21 +7,31 @@ from player import Player
 # throughout this file
 import pygame
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     dt=0
     clock =pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    updatable.add(player)
+    drawable.add(player)
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for thing in updatable:
+            thing.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
 
+        for thing in drawable:
+            thing.draw(screen)  
+        # player.draw(screen)
         pygame.display.flip() 
         dt=(clock.tick(60))/1000.0
 
