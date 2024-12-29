4b99515e96f2517d523def205708e3dae660bb0f
Ari-Wisc <arinoson1@gmail.com>
2024-12-28 18:55:39 -0600

    drew a triangle

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,6 +1,7 @@
 # import everything from a module
 # into the current file
 from constants import *
+from player import Player
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
@@ -9,14 +10,21 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
+    dt=0
+    clock =pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip() 
+        dt=(clock.tick(60))/1000.0
+
 
 
 
